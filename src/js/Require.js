e0e2e8994d2155712e46e9bca78619d88fcfde81
Eko Mardiatno <ekomardiatno@gmail.com>
2019-09-24 15:56:17 +0700

    Signed-off-by: Eko Mardiatno <ekomardiatno@gmail.com>

--- src/js/Require.js
+++ src/js/Require.js
@@ -1,12 +1,11 @@
 /*!
  * ================================
- * Plugins By Eko Mardiatno.
+ * jQuery Plugins By Eko Mardiatno.
  * instagram @komafx
  * wa.me/6282219299071
  * email: ekomardiatno@gmail.com
  * ================================
  */
-
 if (typeof jQuery === 'undefined') {
   throw new Error('Koma\'s JavaScript requires jQuery')
 } else {
